d831be54b8f4e44b82c9bd5a4ed2b0a4659c625e
nazrin-shah <nazrin.shah@foodpanda.com>
2023-03-20 11:15:19 +0800

    add project

---
+++ pd-capstone-fe/pd_capstone_feApp.swift
@@ -0,0 +1,17 @@
+//
+//  pd_capstone_feApp.swift
+//  pd-capstone-fe
+//
+//  Created by Nazrin Shah on 20/3/23.
+//
+
+import SwiftUI
+
+@main
+struct pd_capstone_feApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
