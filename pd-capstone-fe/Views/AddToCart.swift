ed0e6a484b92677da6291cb1ffe0bbc4ff54c931
kenann <kenann.pang@foodpanda.com>
2023-03-29 23:47:49 +0800

    lock button to end of screen on add to cart page

--- pd-capstone-fe/Views/AddToCart.swift
+++ pd-capstone-fe/Views/AddToCart.swift
@@ -20,55 +20,56 @@ struct AddToCart: View {
     var body: some View {
         let sugarLevels: [String] = ["Normal", "More Sweet", "Less Sweet"]
         let iceLevels: [String] = ["Normal", "More Ice", "Less Ice", "No Ice"]
-        
-        ScrollView {
-            dish.image
-                .resizable()
-                .scaledToFit()
-            VStack(alignment: .leading) {
-                VStack {
-                    Text("Sugar Level")
-                        .font(.headline)
-                    
-                    RadioGroupPicker(selectedIndex: $sugarLevel, titles: sugarLevels, itemSpacing: UIScreen.main.bounds.size.width-130, isButtonAfterTitle: true)
-                        .fixedSize()
-                        .accentColor(Color("cerisered"))
-                }
+        NavigationView {
+            VStack {
+                ScrollView {
+                    dish.image
+                        .resizable()
+                        .scaledToFit()
+                    VStack(alignment: .leading) {
+                        VStack {
+                            Text("Sugar Level")
+                                .font(.headline)
+                            
+                            RadioGroupPicker(selectedIndex: $sugarLevel, titles: sugarLevels, itemSpacing: UIScreen.main.bounds.size.width-130, isButtonAfterTitle: true)
+                                .fixedSize()
+                                .accentColor(Color("cerisered"))
+                        }
+                        
+                        Divider()
                 
-                Divider()
-        
-                VStack {
-                    Text("Ice Level")
-                        .font(.headline)
-                    
-                    RadioGroupPicker(selectedIndex: $iceLevel, titles: iceLevels, itemSpacing: UIScreen.main.bounds.size.width-110, isButtonAfterTitle: true)
-                        .fixedSize()
-                        .accentColor(Color("cerisered"))
-                    
+                        VStack {
+                            Text("Ice Level")
+                                .font(.headline)
+                            
+                            RadioGroupPicker(selectedIndex: $iceLevel, titles: iceLevels, itemSpacing: UIScreen.main.bounds.size.width-110, isButtonAfterTitle: true)
+                                .fixedSize()
+                                .accentColor(Color("cerisered"))
+                            
+                        }
+                    }
                 }
-            }
-
-            Button("Add to Cart") {
-                modelData.order.append(dish.toOrderItem(quantity: 1, remarks: "Sweetness: \(sugarLevels[sugarLevel]), Ice: \(iceLevels[iceLevel])"))
-                print(modelData.order)
-                self.presentationMode.wrappedValue.dismiss()
-            }
-            .font(.headline)
-            .foregroundColor(.white)
-            .padding()
-            .frame(width: 340)
-            .background(Color.cerisered)
-            .cornerRadius(8)
-            
-        }
-        .task {
-            do {
-                let url = URL(string: "http://localhost:8080/dish/\(dishId)")!
-                let (data, _) = try await URLSession.shared.data(from: url)
-                dish = try JSONDecoder().decode(Dish.self, from: data)
-            } catch {
-                let _ = print("http://localhost:8080/dish")
-                dish = Dish()
+                .task {
+                    do {
+                        let url = URL(string: "http://localhost:8080/dish/\(dishId)")!
+                        let (data, _) = try await URLSession.shared.data(from: url)
+                        dish = try JSONDecoder().decode(Dish.self, from: data)
+                    } catch {
+                        let _ = print("http://localhost:8080/dish")
+                        dish = Dish()
+                    }
+                }
+                Button("Add to Cart") {
+                    modelData.order.append(dish.toOrderItem(quantity: 1, remarks: "Sweetness: \(sugarLevels[sugarLevel]), Ice: \(iceLevels[iceLevel])"))
+                    print(modelData.order)
+                    self.presentationMode.wrappedValue.dismiss()
+                }
+                .font(.headline)
+                .foregroundColor(.white)
+                .padding()
+                .frame(width: 340)
+                .background(Color.cerisered)
+                .cornerRadius(8)
             }
         }
     }
